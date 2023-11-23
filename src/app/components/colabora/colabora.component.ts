3247680361478be99ed1a7cf40d35b29d3f6cf8b
matifierro <matiass17@hotmail.com>
2023-11-23 14:08:22 -0300

    pagina quienes somos y colabor con nosotros

---
+++ src/app/components/colabora/colabora.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-colabora',
+  templateUrl: './colabora.component.html',
+  styleUrls: ['./colabora.component.css']
+})
+export class ColaboraComponent {
+
+}
